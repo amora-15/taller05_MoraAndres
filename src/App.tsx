7d0c57f9d099fa8750b3500c7956d45766efa407
Andres Felipe Mora Marceles <amora15@cuc.edu.co>
2024-09-19 11:39:01 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import miLogo from "./assets/img/logobarcelona.png"
+
+
+function App() {
+  
+  return (
+    <>
+    <div>
+      <img src={miLogo} width={300} alt="Escudo de Barcelona" />
+      <h1>Barcelona</h1>
+      <p>Més que un club.</p>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
